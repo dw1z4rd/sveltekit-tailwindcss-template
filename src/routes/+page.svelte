b568767ec383f7e2788610bb57b618fb4c45647a
dw1z4rd <toolna@gmail.com>
2023-01-17 19:57:04 -0500

    Sveltekit template with tailwindcss installed

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="w-3/4 mx-auto mt-5 border-4 border-slate-600 rounded-full min-h-96 bg-white">
+  <h1 class="text-3xl font-bold text-center mx-auto mb-4 p-2">Welcome to SvelteKit</h1>
+  <p class="text-lg text-justify m-4 p-4">
+    Visit <a class="hover:text-orange-600 text-blue-500" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+  </p>
+</div>
+
